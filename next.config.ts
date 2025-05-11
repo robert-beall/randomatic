9f892e15a7b213280190439f5d7052cccc764e75
Robert J Beall <rbeall@smcm.edu>
2025-05-11 11:17:27 -0400

    Add basic layout

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+import pkg from "./package.json";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  publicRuntimeConfig: {
+    version: pkg.version,
+  },
 };
 
 export default nextConfig;
